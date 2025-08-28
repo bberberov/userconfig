27ba73441e4524f3116dec5e41140384b9e13331
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2025-08-28 11:54:51 -0600

    Updated `HOME/bashrc.d/prompt.bash`: system users color, `bash` compatibility

--- HOME/bashrc.d/prompt.bash
+++ HOME/bashrc.d/prompt.bash
@@ -2,38 +2,45 @@
 userconfig_use_color=false
 
 case "${TERM}" in
-	[aEkx]term*|rxvt*|gnome*|konsole*|linux|screen|tmux|cons25|*color) userconfig_use_color=true;;
+	[aEkx]term*|rxvt*|gnome*|konsole*|linux|screen|tmux|cons25|*color)
+		userconfig_use_color=true
+
+		if   (( ${EUID} < 1000 ))
+		then
+			userconfig_user_color='\[\e[01;31m\]'
+			userconfig_prompt_color='\[\e[01;31m\]'
+		else
+			userconfig_user_color='\[\e[01;32m\]'
+			userconfig_prompt_color='\[\e[01;34m\]'
+		fi
+	;;
 esac
 
 if   (( 4 == BASH_VERSINFO[0] && 2 <= BASH_VERSINFO[1] \
      || 4  < BASH_VERSINFO[0] \
      ))
 then
-	if ${userconfig_use_color}
+	# Unicode, '+=' operator in 3.1, String expansion $'xxx' in 2.1
+	if   ${userconfig_use_color}
 	then
 		PS0=$'\[\e[01;33m\]\u25b6 \\t \u25b6\[\e[0m\]\n'
 		PS1=$'\[\e[01;33m\]\u2500 \\t \u25c0\[\e[01;35m\] $? \[\e[0m\]'
-		PS1+='\[\e[01;32m\]\u\[\e[01;30m\]@\[\e[01;36m\]\h\[\e[01;30m\]:\[\e[01;34m\]\w\[\e[0m\]\n'
-		PS1+=$'\[\e[01;34m\]\u25b6\[\e[0m\] '
+		PS1+="${userconfig_user_color}"$'\u\[\e[01;30m\]@\[\e[01;36m\]\h\[\e[01;30m\]:\[\e[01;34m\]\w\[\e[0m\]\n'
+		PS1+="${userconfig_prompt_color}"$'\u25b6\[\e[0m\] '
 	else
 		PS0=$'\u25b6 \\t \u25b6\n'
 		PS1=$'\u2500 \\t \u25c0 $? '
-		PS1+='\u@\h:\w\n'
+		PS1+=$'\u@\h:\w\n'
 		PS1+=$'\u25b6 '
 	fi
 else
-	if ${userconfig_use_color}
+	# Bash should support '\n' for PS? variables in all versions?
+	if   ${userconfig_use_color}
 	then
-		PS0=$'\[\e[01;33m\]> \\t >\[\e[0m\]\n'
-		PS1=$'\[\e[01;33m\]- \\t <\[\e[01;35m\] $? \[\e[0m\]'
-		PS1+='\[\e[01;32m\]\u\[\e[01;30m\]@\[\e[01;36m\]\h\[\e[01;30m\]:\[\e[01;34m\]\w\[\e[0m\]\n'
-		PS1+=$'\[\e[01;34m\]>\[\e[0m\] '
+		PS0='\[\e[01;33m\]> \\t >\[\e[0m\]\n'
+		PS1='\[\e[01;33m\]- \\t <\[\e[01;35m\] $? \[\e[0m\]'"${userconfig_user_color}"'\u\[\e[01;30m\]@\[\e[01;36m\]\h\[\e[01;30m\]:\[\e[01;34m\]\w\[\e[0m\]\n'"${userconfig_prompt_color}"'>\[\e[0m\] '
 	else
-		PS0=$'> \\t >\n'
-		PS1=$'- \\t < $? '
-		PS1+='\u@\h:\w\n'
-		PS1+=$'> '
+		PS0='> \t >\n'
+		PS1='- \t < $? \u@\h:\w\n> '
 	fi
 fi
-
-unset userconfig_use_color
