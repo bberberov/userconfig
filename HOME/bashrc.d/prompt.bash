e835c68cd5709baa3f29617ba43f8212a85355fa
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2025-10-27 14:35:56 -0600

    Updated `HOME/bashrc.d/prompt.bash`: `NO_PII` support

--- HOME/bashrc.d/prompt.bash
+++ HOME/bashrc.d/prompt.bash
@@ -25,12 +25,22 @@ then
 	then
 		PS0=$'\[\e[01;33m\]\u25b6 \\t \u25b6\[\e[0m\]\n'
 		PS1=$'\[\e[01;33m\]\u2500 \\t \u25c0\[\e[01;35m\] $? \[\e[0m\]'
+		if [[ -v NO_PII ]]
+		then
+		PS1+="${userconfig_user_color}"$'\u\[\e[01;30m\]@\[\e[01;36m\]test.test\[\e[01;30m\]:\[\e[01;34m\]\W\[\e[0m\]\n'
+		else
 		PS1+="${userconfig_user_color}"$'\u\[\e[01;30m\]@\[\e[01;36m\]\h\[\e[01;30m\]:\[\e[01;34m\]\w\[\e[0m\]\n'
+		fi
 		PS1+="${userconfig_prompt_color}"$'\u25b6\[\e[0m\] '
 	else
 		PS0=$'\u25b6 \\t \u25b6\n'
 		PS1=$'\u2500 \\t \u25c0 $? '
+		if [[ -v NO_PII ]]
+		then
+		PS1+=$'\u@test.test:\W\n'
+		else
 		PS1+=$'\u@\h:\w\n'
+		fi
 		PS1+=$'\u25b6 '
 	fi
 else
@@ -38,9 +48,19 @@ else
 	if   ${userconfig_use_color}
 	then
 		PS0='\[\e[01;33m\]> \\t >\[\e[0m\]\n'
+		if   [[ -n "${NO_PII}" ]]
+		then
+		PS1='\[\e[01;33m\]- \\t <\[\e[01;35m\] $? \[\e[0m\]'"${userconfig_user_color}"'\u\[\e[01;30m\]@\[\e[01;36m\]test.test\[\e[01;30m\]:\[\e[01;34m\]\W\[\e[0m\]\n'"${userconfig_prompt_color}"'>\[\e[0m\] '
+		else
 		PS1='\[\e[01;33m\]- \\t <\[\e[01;35m\] $? \[\e[0m\]'"${userconfig_user_color}"'\u\[\e[01;30m\]@\[\e[01;36m\]\h\[\e[01;30m\]:\[\e[01;34m\]\w\[\e[0m\]\n'"${userconfig_prompt_color}"'>\[\e[0m\] '
+		fi
 	else
 		PS0='> \t >\n'
+		if   [[ -n "${NO_PII}" ]]
+		then
+		PS1='- \t < $? \u@test.test:\W\n> '
+		else
 		PS1='- \t < $? \u@\h:\w\n> '
+		fi
 	fi
 fi
