f1ce26ed801372ca71a8f56ce5d512e33b7a3878
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2025-10-27 14:08:25 -0600

    Updated `HOME/bashrc.d/aliases.bash`: `mkfs.btrfs*`, `mkfs.exfat*`, `*.f2fs*`, `mkfs.fat*` aliases

--- HOME/bashrc.d/aliases.bash
+++ HOME/bashrc.d/aliases.bash
@@ -678,6 +678,75 @@ then
 	alias   lsblk-fs='lsblk  -p -o NAME,TYPE,FSTYPE,SIZE,FSSIZE,FSUSED,FSAVAIL,FSUSE%,LABEL,PARTLABEL,MOUNTPOINTS'
 fi
 
+# mkfs.btrfs
+if   which mkfs.btrfs > /dev/null 2>&1
+then
+	alias mkfs.btrfs-config-single='mkfs.btrfs \
+--data=single \
+--metadata=single \
+'
+	alias mkfs.btrfs-config-dup='mkfs.btrfs \
+--data=dup \
+--metadata=dup \
+'
+	alias mkfs.btrfs-list-all='mkfs.btrfs -O list-all'
+elif [[ -f '/usr/sbin/mkfs.btrfs' ]]
+then
+	alias mkfs.btrfs-list-all='/usr/sbin/mkfs.btrfs -O list-all'
+fi
+
+# mkfs.exfat
+if   which mkfs.exfat > /dev/null 2>&1
+then
+	alias   mkfs.exfat-config-sd-8MiB='mkfs.exfat -b  8k -c   8k'
+	alias  mkfs.exfat-config-sd-64MiB='mkfs.exfat -b 16k -c  16k'
+	alias mkfs.exfat-config-sd-256MiB='mkfs.exfat -b 32k -c  16k'
+	alias   mkfs.exfat-config-sd-1GiB='mkfs.exfat -b 64k -c  16k'
+	alias   mkfs.exfat-config-sd-2GiB='mkfs.exfat -b 64k -c  32k'
+	alias  mkfs.exfat-config-sd-32GiB='mkfs.exfat -b  4m -c  32k'
+	alias mkfs.exfat-config-sd-128GiB='mkfs.exfat -b 16m -c 128k'
+	alias mkfs.exfat-config-sd-512GiB='mkfs.exfat -b 32m -c 256k'
+	alias   mkfs.exfat-config-sd-2TiB='mkfs.exfat -b 64m -c 512k'
+fi
+
+# mkfs.f2fs
+if   which mkfs.f2fs > /dev/null 2>&1
+then
+	alias mkfs.f2fs-config-default='mkfs.btrfs \
+	-e \
+jp,mp,og,wm,\
+avi,m4p,m4v,mkv,mov,webm,\
+3gp,flac,m4a,opus,wav,\
+gif,png,svg,webp,\
+7z,bz2,deb,gz,iso,jar,rar,rpm,tar,xz,zst,\
+pdf,pyc,ttc,ttf,exe,\
+apk,cnt,exo,odex,vdex,so\
+	-E \
+db,sqlite,\
+qcow2,vdi,vmdk\
+	-O compression,extra_attr,inode_checksum,flexible_inline_xattr,sb_checksum
+'
+fi
+
+if   ! which mount.f2fs > /dev/null 2>&1
+then
+	alias mount.f2fs='mount -t f2fs \
+	-o \
+atgc,
+compress_algorithm=zstd:6,\
+compress_extension=txt,
+compress_chksum,\
+'
+fi
+
+# mkfs.fat
+if   which mkfs.fat > /dev/null 2>&1
+then
+	which mkfs.fat12 > /dev/null 2>&1 || alias mkfs.fat12='mkfs.fat -F 12'
+	which mkfs.fat16 > /dev/null 2>&1 || alias mkfs.fat12='mkfs.fat -F 16'
+	which mkfs.fat32 > /dev/null 2>&1 || alias mkfs.fat12='mkfs.fat -F 32'
+fi
+
 # npm
 if   which npm > /dev/null 2>&1
 then
