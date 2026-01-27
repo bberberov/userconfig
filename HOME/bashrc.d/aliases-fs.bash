35265c18f29f65f1e603add7172d47b17bf3bc70
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2026-01-27 14:06:48 -0600

    Updated `HOME/bashrc.d/aliases-fs.bash`: new aliases, fixed old aliases

--- HOME/bashrc.d/aliases-fs.bash
+++ HOME/bashrc.d/aliases-fs.bash
@@ -5,14 +5,25 @@
 # mkfs.btrfs
 if   which mkfs.btrfs > /dev/null 2>&1
 then
-	alias mkfs.btrfs-config-single='mkfs.btrfs \
---data=single \
---metadata=single \
+	alias mkfs.btrfs-config_single='mkfs.btrfs \
+--data single \
+--metadata single \
 '
-	alias mkfs.btrfs-config-dup='mkfs.btrfs \
---data=dup \
---metadata=dup \
+	alias mkfs.btrfs-config_dup='mkfs.btrfs \
+--data dup \
+--metadata dup \
 '
+	alias mkfs.btrfs-config_storage='mkfs.btrfs \
+--data single \
+--metadata single \
+--nodesize 64K \
+'
+	alias mkfs.btrfs-config_archive-dup='mkfs.btrfs \
+--data dup \
+--metadata dup \
+--nodesize 64K \
+'
+
 	alias mkfs.btrfs-list-all='mkfs.btrfs -O list-all'
 elif [[ -f '/usr/sbin/mkfs.btrfs' ]]
 then
@@ -26,15 +37,15 @@ fi
 # mkfs.exfat
 if   which mkfs.exfat > /dev/null 2>&1
 then
-	alias   mkfs.exfat-config-sd-8MiB='mkfs.exfat -b  8k -c   8k'
-	alias  mkfs.exfat-config-sd-64MiB='mkfs.exfat -b 16k -c  16k'
-	alias mkfs.exfat-config-sd-256MiB='mkfs.exfat -b 32k -c  16k'
-	alias   mkfs.exfat-config-sd-1GiB='mkfs.exfat -b 64k -c  16k'
-	alias   mkfs.exfat-config-sd-2GiB='mkfs.exfat -b 64k -c  32k'
-	alias  mkfs.exfat-config-sd-32GiB='mkfs.exfat -b  4m -c  32k'
-	alias mkfs.exfat-config-sd-128GiB='mkfs.exfat -b 16m -c 128k'
-	alias mkfs.exfat-config-sd-512GiB='mkfs.exfat -b 32m -c 256k'
-	alias   mkfs.exfat-config-sd-2TiB='mkfs.exfat -b 64m -c 512k'
+	alias   mkfs.exfat-config_sd-8MiB='mkfs.exfat -b  8K -c   8K'
+	alias  mkfs.exfat-config_sd-64MiB='mkfs.exfat -b 16K -c  16K'
+	alias mkfs.exfat-config_sd-256MiB='mkfs.exfat -b 32K -c  16K'
+	alias   mkfs.exfat-config_sd-1GiB='mkfs.exfat -b 64K -c  16K'
+	alias   mkfs.exfat-config_sd-2GiB='mkfs.exfat -b 64K -c  32K'
+	alias  mkfs.exfat-config_sd-32GiB='mkfs.exfat -b  4M -c  32K'
+	alias mkfs.exfat-config_sd-128GiB='mkfs.exfat -b 16M -c 128K'
+	alias mkfs.exfat-config_sd-512GiB='mkfs.exfat -b 32M -c 256K'
+	alias   mkfs.exfat-config_sd-2TiB='mkfs.exfat -b 64M -c 512K'
 fi
 
 # END   exFAT
@@ -45,17 +56,17 @@ fi
 if   which tune2fs > /dev/null 2>&1
 then
 	#                                 mount_opts limit: [------8-------+-------+-------|-------+-------+-------+------]
-	alias    tune2fs-config-root='tune2fs -E mount_opts=noatime'
-	alias    tune2fs-config-boot='tune2fs -E mount_opts=noatime,nodev,nosuid,noexec'
-	alias     tune2fs-config-opt='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
-	alias     tune2fs-config-usr='tune2fs -E mount_opts=noatime,nodev,commit=20'
-	alias     tune2fs-config-var='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
-	alias  tune2fs-config-varlib='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
-	alias  tune2fs-config-vartmp='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
-	alias    tune2fs-config-home='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
-	alias     tune2fs-config-srv='tune2fs -E mount_opts=noatime,nodev,nosuid,noexec,commit=20'
+	alias    tune2fs-config_root='tune2fs -E mount_opts=noatime'
+	alias    tune2fs-config_boot='tune2fs -E mount_opts=noatime,nodev,nosuid,noexec'
+	alias     tune2fs-config_opt='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
+	alias     tune2fs-config_usr='tune2fs -E mount_opts=noatime,nodev,commit=20'
+	alias     tune2fs-config_var='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
+	alias  tune2fs-config_varlib='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
+	alias  tune2fs-config_vartmp='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
+	alias    tune2fs-config_home='tune2fs -E mount_opts=noatime,nodev,nosuid,commit=20'
+	alias     tune2fs-config_srv='tune2fs -E mount_opts=noatime,nodev,nosuid,noexec,commit=20'
 
-	alias tune2fs-config-storage='tune2fs -E mount_opts=noatime,nodev,nosuid,noexec'
+	alias tune2fs-config_storage='tune2fs -E mount_opts=noatime,nodev,nosuid,noexec'
 fi
 
 # END   EXT4
@@ -65,7 +76,7 @@ fi
 # mkfs.f2fs
 if   which mkfs.f2fs > /dev/null 2>&1
 then
-	alias mkfs.f2fs-config-default='mkfs.btrfs \
+	alias mkfs.f2fs-config_default='mkfs.btrfs \
 	-e \
 jp,mp,og,wm,\
 avi,m4p,m4v,mkv,mov,webm,\
@@ -100,11 +111,60 @@ fi
 # mkfs.fat
 if   which mkfs.fat > /dev/null 2>&1
 then
-	which mkfs.fat12 > /dev/null 2>&1 || alias mkfs.fat12='mkfs.fat -F 12'
-	which mkfs.fat16 > /dev/null 2>&1 || alias mkfs.fat16='mkfs.fat -F 16'
-	alias mkfs.fat16-af='mkfs.fat -F 16 -S 4096'
-	which mkfs.fat32 > /dev/null 2>&1 || alias mkfs.fat32='mkfs.fat -F 32'
-	alias mkfs.fat32-af='mkfs.fat -F 32 -S 4096'
+	if   which mkfs.fat12 > /dev/null 2>&1
+	then
+		alias mkfs.fat12='mkfs.fat -F 12'
+	fi
+
+	if   which mkfs.fat16 > /dev/null 2>&1
+	then
+		alias mkfs.fat16='mkfs.fat -F 16'
+
+		alias     mkfs.fat16-config_sf='mkfs.fat -F 16 -S 512'
+		alias mkfs.fat16-config_sf-512='mkfs.fat -F 16 -S 512 -s 1'
+		alias  mkfs.fat16-config_sf-1k='mkfs.fat -F 16 -S 512 -s 2'
+		alias  mkfs.fat16-config_sf-2k='mkfs.fat -F 16 -S 512 -s 4'
+		alias  mkfs.fat16-config_sf-4k='mkfs.fat -F 16 -S 512 -s 8'
+		alias  mkfs.fat16-config_sf-8k='mkfs.fat -F 16 -S 512 -s 16'
+		alias mkfs.fat16-config_sf-16k='mkfs.fat -F 16 -S 512 -s 32'
+		alias mkfs.fat16-config_sf-32k='mkfs.fat -F 16 -S 512 -s 64'
+		alias mkfs.fat16-config_sf-64k='mkfs.fat -F 16 -S 512 -s 128'
+
+		alias      mkfs.fat16-config_af='mkfs.fat -F 16 -S 4096'
+		alias   mkfs.fat16-config_af-4k='mkfs.fat -F 16 -S 4096 -s 1'
+		alias   mkfs.fat16-config_af-8k='mkfs.fat -F 16 -S 4096 -s 2'
+		alias  mkfs.fat16-config_af-16k='mkfs.fat -F 16 -S 4096 -s 4'
+		alias  mkfs.fat16-config_af-32k='mkfs.fat -F 16 -S 4096 -s 8'
+		alias  mkfs.fat16-config_af-64k='mkfs.fat -F 16 -S 4096 -s 16'
+		alias mkfs.fat16-config_af-128k='mkfs.fat -F 16 -S 4096 -s 32'
+		alias mkfs.fat16-config_af-256k='mkfs.fat -F 16 -S 4096 -s 64'
+		alias mkfs.fat16-config_af-512k='mkfs.fat -F 16 -S 4096 -s 128'
+	fi
+
+	if   which mkfs.fat32 > /dev/null 2>&1
+	then
+		alias mkfs.fat32='mkfs.fat -F 32'
+
+		alias     mkfs.fat32-config_sf='mkfs.fat -F 32 -S 512'
+		alias mkfs.fat32-config_sf-512='mkfs.fat -F 32 -S 512 -s 1'
+		alias  mkfs.fat32-config_sf-1k='mkfs.fat -F 32 -S 512 -s 2'
+		alias  mkfs.fat32-config_sf-2k='mkfs.fat -F 32 -S 512 -s 4'
+		alias  mkfs.fat32-config_sf-4k='mkfs.fat -F 32 -S 512 -s 8'
+		alias  mkfs.fat32-config_sf-8k='mkfs.fat -F 32 -S 512 -s 16'
+		alias mkfs.fat32-config_sf-16k='mkfs.fat -F 32 -S 512 -s 32'
+		alias mkfs.fat32-config_sf-32k='mkfs.fat -F 32 -S 512 -s 64'
+		alias mkfs.fat32-config_sf-64k='mkfs.fat -F 32 -S 512 -s 128'
+
+		alias      mkfs.fat32-config_af='mkfs.fat -F 32 -S 4096'
+		alias   mkfs.fat32-config_af-4k='mkfs.fat -F 32 -S 4096 -s 1'
+		alias   mkfs.fat32-config_af-8k='mkfs.fat -F 32 -S 4096 -s 2'
+		alias  mkfs.fat32-config_af-16k='mkfs.fat -F 32 -S 4096 -s 4'
+		alias  mkfs.fat32-config_af-32k='mkfs.fat -F 32 -S 4096 -s 8'
+		alias  mkfs.fat32-config_af-64k='mkfs.fat -F 32 -S 4096 -s 16'
+		alias mkfs.fat32-config_af-128k='mkfs.fat -F 32 -S 4096 -s 32'
+		alias mkfs.fat32-config_af-256k='mkfs.fat -F 32 -S 4096 -s 64'
+		alias mkfs.fat32-config_af-512k='mkfs.fat -F 32 -S 4096 -s 128'
+	fi
 fi
 
 # END   FAT
