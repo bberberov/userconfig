945fdb24028610bdaa58621f9739107bc149fd4a
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2026-09-21 23:13:04 -0600

    Added browser system configuration

---
+++ etc/vulpes/defaults/pref/local.js
@@ -0,0 +1,4 @@
+pref("general.config.filename",        "local.cfg", locked);
+pref("general.config.obscure_value",   0,           locked);
+pref("general.config.sandbox_enabled",  true,       locked);
+pref("general.config.vendor",          "local",     locked);
